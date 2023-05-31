92e02039c9ddd79e56448e370e2f9a13b96caf76
Daniel Farr <hi@drfarr.uk>
2023-05-31 09:17:14 +0100

    :white_check_mark: Adding 2/3 passing tests for weather api

--- tests/app/api/weather/route.test.ts
+++ tests/app/api/weather/route.test.ts
@@ -1,19 +1,23 @@
-describe("/api/weather Endpoint", () => {
-  it("should return a successful response", async () => {
-    const res = null;
-    expect(res.statusCode).toBe(200);
-    expect(res.getHeaders()).toEqual({ "content-type": "application/json" });
-    expect(res.statusMessage).toEqual("OK");
-  });
+import { test, expect } from "@playwright/test";
 
-  it("should return a 400 if location is missing", async () => {
-    const res = null;
-    expect(res.statusCode).toBe(400);
-    expect(res.statusMessage).toEqual("OK");
-  });
+test("should return a 400 if location is missing", async ({ request }) => {
+  const response = await request.get(`/api/weather`, {});
+  const { data } = await response.json();
+  expect(response.ok()).toBeFalsy();
+  expect(response.status()).toBe(400);
+  expect(data).toHaveLength(0);
+});
 
-  it("should return a 405 if HTTP method is not GET", async () => {
-    const res = null;
-    expect(res.statusCode).toBe(405);
-  });
+test("should return a successful response", async ({ request }) => {
+  const response = await request.get(`/api/weather?q=brighton`, {});
+  const { data } = await response.json();
+  expect(response.ok()).toBeTruthy();
+  expect(response.status()).toBe(200);
+  expect(data).toHaveProperty("location");
+});
+
+test("should return a 405 if HTTP method is not GET", async ({ request }) => {
+  const response = await request.post(`/api/weather?q=brighton`, {});
+  expect(response.ok()).toBeFalsy();
+  expect(response.status()).toBe(405);
 });
