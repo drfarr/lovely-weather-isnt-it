527d898eb82448d249ff9d4896f7a9a71938e336
Daniel Farr <hi@drfarr.uk>
2023-05-31 16:24:25 +0100

    :wrench: Updating playwright and tscofnig
    so that playwright will pikc up patth aliases

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
+  testMatch: "*/*.test.ts",
   use: {
     baseURL: "http://localhost:3000",
     extraHTTPHeaders: {
       "Content-Type": "application/json",
     },
+    actionTimeout: 30 * 1000,
+    navigationTimeout: 120 * 1000,
+
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
   },
 });
