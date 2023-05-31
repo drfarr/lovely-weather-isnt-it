2e9d90658b57dd31193c0a542b8f35eb428972ec
Daniel Farr <hi@drfarr.uk>
2023-05-31 12:57:59 +0100

    :necktie: Implementing loading astates
    Aswell as cleaning up some UOI implementing progress bars and a few small cleaniup bits

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import Card from "@/components/Card";
+import Loader from "@/components/Loader";
+import Progress from "@/components/Progress";
 import useWeather from "@/hooks/useWeather";
 import { formatDate, formatHours, formatTempreature } from "@/utils";
 import { Unit } from "@/utils/WeatherAPI.class";
 import { IWeatherResource } from "@/utils/WeatherAdapter.class";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 interface FormElements extends HTMLFormControlsCollection {
   queryInput: HTMLInputElement;
@@ -16,23 +18,18 @@ interface QueryFormElement extends HTMLFormElement {
 
 export default function Home() {
   const [query, setQuery] = useState<string>("brighton");
-  const { data, error, loading } = useWeather<IWeatherResource>(query);
-  const [displayUnits, setDisplayUnits] = useState(data?.units);
+  const { data: _data, error, loading } = useWeather<IWeatherResource>(query);
+  const [displayUnits, setDisplayUnits] = useState(_data?.units);
+
+  const data: IWeatherResource | null = useMemo(() => {
+    return _data?.name && !loading ? _data : null;
+  }, [_data, loading]);
 
   useEffect(() => {
-    setDisplayUnits(data?.units);
+    if (data?.units) {
+      setDisplayUnits(data?.units);
+    }
   }, [data?.units]);
-  if (error) {
-    return "Something went wrong";
-  }
-
-  if (loading) {
-    return "Loading";
-  }
-
-  if (!data?.name && !loading) {
-    return "No data";
-  }
 
   const handleSubmit = (e: React.SyntheticEvent<QueryFormElement>): void => {
     e.preventDefault();
@@ -42,7 +39,7 @@ export default function Home() {
   };
 
   const formatTemp = (value: number) => {
-    return formatTempreature(value, data?.units ?? Unit.metric);
+    return formatTempreature(value, displayUnits ?? Unit.metric);
   };
 
   return (
@@ -65,6 +62,7 @@ export default function Home() {
                   placeholder="Search"
                 />
                 <button
+                  disabled={loading || !query}
                   type="submit"
                   className="text-white absolute right-2.5 bottom-2.5 bg-white hover:bg-teritary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
@@ -87,7 +85,14 @@ export default function Home() {
             </form>
           </div>
 
-          {data && (
+          {error ? (
+            <div className="w-full h-full">
+              <h1 className="text-3xl m-10">Oops something went wrong :(</h1>
+              <h1 className="text-2xl m-10">Please try again later.</h1>
+            </div>
+          ) : loading ? (
+            <Loader />
+          ) : data?.address && !loading ? (
             <ul className="space-y-6 font-medium text-center mt-20">
               <li>
                 <h1 className="text-3xl">{data.address}</h1>
@@ -111,82 +116,106 @@ export default function Home() {
                 <h1 className="text-2xl">{data.conditions}</h1>
               </li>
             </ul>
+          ) : (
+            <div className="w-full h-full">
+              <h1 className="text-2xl m-10">
+                No results found for <i>{query}</i>
+              </h1>
+              <h1 className="text-xl m-10">Please try another search</h1>
+            </div>
           )}
         </div>
       </aside>
 
       <div className="p-4 sm:ml-64 bg-secondary h-screen ">
-        <div className="w-2/3 mx-auto">
-          <div className="p-4 flex flex-row-reverse ">
-            {Object.keys(Unit).map((unit, idx) => {
-              const classes =
-                unit === displayUnits
-                  ? "bg-white text-black"
-                  : "bg-teritary text-white";
-              return (
-                <a
-                  onClick={() => {
-                    setDisplayUnits(unit as Unit);
-                  }}
-                  key={idx}
-                  className={`${classes} cursor-pointer rounded-full ml-2 p-2 w-10 h-9"`}
-                >
-                  °{unit === Unit.metric ? "C" : "F"}
-                </a>
-              );
-            })}
-          </div>
-          <div className="p-4">
-            <h2 className="text-2xl mb-4">Daily Overview</h2>
-            {data && (
-              <div>
-                <div className="grid grid-cols-2 gap-4 mb-4">
-                  <Card title="Humdity" value={data.humidity} />
-                  <Card title="Cloud Cover" value={data.cloudCover} />
-                </div>
-                <div className="grid grid-cols-4 gap-4 mb-4">
-                  <Card title="Min Temp" value={formatTemp(data.minTemp)} />
-                  <Card title="Max Temp" value={formatTemp(data.maxTemp)} />
-                  <Card title="Sunrise" value={formatHours(data.sunRiseTime)} />
-                  <Card title="Sunset" value={formatHours(data.sunSetTime)} />
-                </div>
-              </div>
-            )}
-          </div>
-
-          <div className="p-4 inline-flex w-full flex-col">
-            <h2 className="text-2xl mb-4 h-24">
-              {data?.days?.length} Day Forecast
-            </h2>
-
-            <div className="grid grid-cols-5 gap-4 mb-4">
-              {data &&
-                data.days.length &&
-                data.days.map((data, idx) => {
+        {(!loading && !data?.name) ||
+          (!error && (
+            <div className="w-2/3 mx-auto">
+              <div className="p-4 flex flex-row-reverse ">
+                {Object.keys(Unit).map((unit, idx) => {
+                  const classes =
+                    unit === displayUnits
+                      ? "bg-white text-black"
+                      : "bg-teritary text-white";
                   return (
-                    <div
+                    <a
+                      onClick={() => {
+                        setDisplayUnits(unit as Unit);
+                      }}
                       key={idx}
-                      className="flex items-center justify-center h-24 rounded bg-primary dark:bg-gray-800"
+                      className={`${classes} cursor-pointer rounded-full ml-2 p-2 w-10 h-9"`}
                     >
-                      <Card
-                        tall
-                        title={formatDate(data.date)}
-                        value={`${formatTemp(data.maxTemp)} / 
-                        ${formatTemp(data.minTemp)}`}
-                      >
-                        <Image
-                          alt={data.icon}
-                          width={100}
-                          height={50}
-                          src={`/icons/${"cloudy"}.svg`}
-                        />
-                      </Card>
-                    </div>
+                      °{unit === Unit.metric ? "C" : "F"}
+                    </a>
                   );
                 })}
+              </div>
+              <div className="p-4">
+                <h2 className="text-2xl mb-4">Daily Overview</h2>
+                {loading ? (
+                  <Loader />
+                ) : data?.address && !loading ? (
+                  <div>
+                    <div className="grid grid-cols-2 gap-4 mb-4">
+                      <Card title="Humdity" value={`${data.humidity}%`}>
+                        <Progress value={data.humidity ?? 0} />
+                      </Card>
+                      <Card title="Cloud Cover" value={`${data.cloudCover}%`}>
+                        <Progress value={data.cloudCover ?? 0} />
+                      </Card>
+                    </div>
+                    <div className="grid grid-cols-4 gap-4 mb-4">
+                      <Card title="Min Temp" value={formatTemp(data.minTemp)} />
+                      <Card title="Max Temp" value={formatTemp(data.maxTemp)} />
+                      <Card
+                        title="Sunrise"
+                        value={formatHours(data.sunRiseTime)}
+                      />
+                      <Card
+                        title="Sunset"
+                        value={formatHours(data.sunSetTime)}
+                      />
+                    </div>
+                  </div>
+                ) : null}
+              </div>
+
+              <div className="p-4 inline-flex w-full flex-col">
+                <h2 className="text-2xl mb-4 h-24">
+                  {data?.days?.length} Day Forecast
+                </h2>
+
+                <div className="grid grid-cols-5 gap-4 mb-4">
+                  {loading ? (
+                    <Loader />
+                  ) : data && data.days.length ? (
+                    data.days.map((data, idx) => {
+                      return (
+                        <div
+                          key={idx}
+                          className="flex items-center justify-center h-24 rounded bg-primary dark:bg-gray-800"
+                        >
+                          <Card
+                            tall
+                            title={formatDate(data.date)}
+                            value={`${formatTemp(data.maxTemp)} / 
+                        ${formatTemp(data.minTemp)}`}
+                          >
+                            <Image
+                              alt={data.icon}
+                              width={100}
+                              height={50}
+                              src={`/icons/${"cloudy"}.svg`}
+                            />
+                          </Card>
+                        </div>
+                      );
+                    })
+                  ) : null}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          ))}
       </div>
     </main>
   );
